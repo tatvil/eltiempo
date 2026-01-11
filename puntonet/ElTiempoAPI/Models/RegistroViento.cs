53c98e47d00a863f266878f36d2ae7d32054b6b5
Tatiana Villa Ema <tatiana@tecnologia-facil.es>
2026-01-11 12:05:11 +0100

    Estadisticas por ciudad

---
+++ puntonet/ElTiempoAPI/Models/RegistroViento.cs
@@ -0,0 +1,11 @@
+﻿namespace ElTiempoAPI.Models
+{
+    public class RegistroViento
+    {
+        public DateTime Fecha { get; set; }
+        public double VientoVelocidad { get; set; }
+        public double VientoDireccion { get; set; }
+        public bool EsLunaLlena { get; set; }
+    }
+}
+
